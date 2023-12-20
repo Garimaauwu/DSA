d57e210f2343d9f247956dcf766f72df5038a097
G <90741479+Garimaauwu@users.noreply.github.com>
2023-12-20 21:33:01 +0530

    Time: 4 ms (93.98%) | Memory: 47 MB (58.30%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
         sort(prices.begin(), prices.end());
-        
-        if(money-(prices[0]+prices[1])>=0)
+               
+
+        if(money-(prices[0]+prices[1])<0)
+          return money ; 
         return money-(prices[0]+prices[1]);
-        else 
-        return money ; 
+        
+        
     }
 };
