f2987195f2e4187405bb1f0f0b96101345ae3334
G <90741479+Garimaauwu@users.noreply.github.com>
2023-12-23 00:51:52 +0530

    Time: 0 ms (100.00%) | Memory: 6.7 MB (62.95%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -4,23 +4,12 @@ public:
       int n= s.length(); 
       int result =INT_MIN; 
       int totalone=count(s.begin(),s.end(),'1');
+      int zero=0, leftone=0; 
       for(int i=0;i<=n-2;i++)
-      {
-          int zero=0;
-          int leftone=0; 
-          for(int j=0;j<=i;j++)
-          {
-                if(s[j]=='0')
-                {
-                    zero++; 
-                }
-                
-              if(s[j]=='1')
-              {
-                  leftone++; 
-              }
-          }
-         
+      {  if(s[i]=='0')
+                  zero++;
+                    else   
+                leftone++; 
           result = max(result,zero+(totalone-leftone));
       }  
       return result ; 
