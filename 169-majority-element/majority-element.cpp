5997a52aa3f6b3219f1742b8257e55e6d925b143
Garima <90741479+Garimaauwu@users.noreply.github.com>
2024-01-27 00:03:47 +0530

    Time: 8 ms (92.93%) | Memory: 22.2 MB (6.62%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int,int> mapp;
+        int maxi = 0;
+        int n = nums.size(); 
+        for (int i = 0; i < n; i++) {
+             mapp[nums[i]]++; }
+              for (auto it : mapp) {
+        if (it.second > (n / 2)) {
+            return it.first;
+        }
+    }
+
+    return -1;
+}
+    
+};
