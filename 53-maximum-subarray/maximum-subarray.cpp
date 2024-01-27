fa3b336de4e03d8e2669aca8c0b1b4eb6055b4b6
Garima <90741479+Garimaauwu@users.noreply.github.com>
2024-01-27 12:33:29 +0530

    Time: 93 ms (26.10%) | Memory: 70.3 MB (9.34%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int n = nums.size();
-        int maxsum = nums[0];
-        int sum = nums[0];     
+       int n = nums.size();
+        int sum = 0;
+        int maxi = nums[0];  // Initialize with the first element
 
-        for (int i = 1; i < n; i++) {
-           
-            sum = max(nums[i], sum + nums[i]);
-            
-            
-            maxsum = max(maxsum, sum);
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+
+            if (sum < nums[i]) {
+                sum = nums[i];
+            }
+
+            // Update maxi with the maximum of current sum and previous maxi
+            maxi = max(maxi, sum);
         }
 
-        return maxsum;
+        return maxi;
     }
+   
 };
