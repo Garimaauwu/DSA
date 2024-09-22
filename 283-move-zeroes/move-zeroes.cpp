b0777363fad5a74595755cad56771eea19d21952
Garima <90741479+Garimaauwu@users.noreply.github.com>
2024-09-22 18:38:11 +0530

    Time: 11 ms (90.67%) | Memory: 22 MB (28.14%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,17 +2,30 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
-        vector<int> temp;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != 0) {
-                temp.push_back(nums[i]);
-            }
-        }
-        for (int i = 0; i < temp.size(); i++) {
-            nums[i] = temp[i];
-        }
-        for (int i = temp.size(); i < n; i++) {
-            nums[i] = 0;
-        }
+int j=-1;
+//this loop is to find out the first index where 0 is present 
+for(int i=0;i<n;i++)
+{
+   
+    if(nums[i]==0)
+    {
+        j=i;
+        break;
+    }
+} 
+if(j==-1)//this means there is not a single index where 0 is present 
+{
+    return ;
+}     
+
+for(int i=j+1;i<n;i++)
+{
+    if(nums[i]!=0)
+    {
+        swap(nums[i],nums[j]);
+        j++;  //j will always be on the index where 0 is present 
+    }
+}
+        
     }
 };
